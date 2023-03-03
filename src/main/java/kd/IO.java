dc50cbf20d3572a4782fc5f6bd63e9c760d85203
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-03 20:44:37 +0100

    implement user/book not found message

--- src/main/java/kd/IO.java
+++ src/main/java/kd/IO.java
@@ -98,8 +98,21 @@ public class IO {
     void issueBook(Scanner scanner, PrintWriter outFile) {
         System.out.printf("%n%s%n%s%n", "ISSUE A BOOK", "--------------");
 
-        User selectedUser = validateUser(scanner);
-        Book selectedBook = validateBook(scanner);
+        User selectedUser;
+        Book selectedBook;
+
+        try {
+            selectedUser = validateUser(scanner);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        try {
+            selectedBook = validateBook(scanner);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
 
         if (selectedUser.getNumOfBooks() < 3) { // Check if user has less than 3 books on loan (can borrow 3 max)
 
@@ -126,8 +139,21 @@ public class IO {
     private void returnBook(Scanner scanner) {
         System.out.printf("%n%s%n%s%n", "RETURN A BOOK", "--------------");
 
-        User selectedUser = validateUser(scanner);
-        Book selectedBook = validateBook(scanner);
+        User selectedUser;
+        Book selectedBook;
+
+        try {
+            selectedUser = validateUser(scanner);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        try {
+            selectedBook = validateBook(scanner);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
 
         if (selectedUser.equals(selectedBook.getBorrower())) { // If book is borrowed by selected user, return book
             selectedBook.setBorrower(null);
@@ -143,7 +169,7 @@ public class IO {
         }
     }
 
-    private User validateUser(Scanner scanner) {
+    private User validateUser(Scanner scanner) throws Exception {
         System.out.println("Enter the user's first name:");
         String userFirstName = scanner.nextLine().trim();
         System.out.println("Enter the user's surname:");
@@ -152,6 +178,7 @@ public class IO {
         User selectedUser = null;
         boolean validUser = false;
 
+        int timesToRetry = 1;
         while (!validUser) {
             for (User u : users) {
                 if (u.getFirstName().equalsIgnoreCase(userFirstName)
@@ -161,17 +188,23 @@ public class IO {
                 }
             }
 
-            if (!validUser) {
-                System.out.println("Invalid user, please enter the user's first name again:");
-                userFirstName = scanner.nextLine().trim();
-                System.out.println("Enter the user's surname:");
-                userSurname = scanner.nextLine().trim();
+            if (timesToRetry > 0) {
+                if (!validUser) {
+                    timesToRetry--;
+                    System.out.println("Invalid user, please enter the user's first name again:");
+                    userFirstName = scanner.nextLine().trim();
+                    System.out.println("Enter the user's surname:");
+                    userSurname = scanner.nextLine().trim();
+                }
+            } else {
+                throw new Exception("User was not found");
             }
+
         }
         return selectedUser;
     }
 
-    private Book validateBook(Scanner scanner) {
+    private Book validateBook(Scanner scanner) throws Exception {
         System.out.println("Enter the book's title:");
         String bookTitle = scanner.nextLine().trim();
         System.out.println("Enter the surname of the book's author:");
@@ -180,6 +213,7 @@ public class IO {
         Book selectedBook = null;
         boolean validBook = false;
 
+        int timesToRetry = 1;
         while (!validBook) {
             for (Book b : books) {
                 if (b.getTitle().equalsIgnoreCase(bookTitle)
@@ -189,11 +223,16 @@ public class IO {
                 }
             }
 
-            if (!validBook) {
-                System.out.println("Invalid book, please enter the book's title again:");
-                bookTitle = scanner.nextLine().trim();
-                System.out.println("Enter the surname of the book's author:");
-                bookAuthorSurname = scanner.nextLine().trim();
+            if (timesToRetry > 0) {
+                if (!validBook) {
+                    timesToRetry--;
+                    System.out.println("Invalid book, please enter the book's title again:");
+                    bookTitle = scanner.nextLine().trim();
+                    System.out.println("Enter the surname of the book's author:");
+                    bookAuthorSurname = scanner.nextLine().trim();
+                }
+            } else {
+                throw new Exception("Book was not found");
             }
         }
         return selectedBook;
